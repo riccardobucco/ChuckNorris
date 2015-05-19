addb35392c6061b60fa6b1e4786d29387d4527b5
ChiaraBi <chiara.bigarella1@gmail.com>
2015-05-19 12:38:28 +0200

    Norris-DataModel: aggiunta documentazione per NorrisImpl

--- Sources/Norris/main/DataModel/NorrisImpl.js
+++ Sources/Norris/main/DataModel/NorrisImpl.js
@@ -55,17 +55,23 @@ NorrisImpl.prototype.setSettings = function(settings) {
     }
 };
 
+/**
+ * NorrisImpl.prototype.getSettings allows you to get the Norris' settings.
+ *
+ * @return 	Norris' settings.
+ */
 NorrisImpl.prototype.getSettings = function() {
     return this.settings;
 };
 
 /**
  * NorrisImpl.createChart allows you to create a new chart and adds it to the Norris instance's list of charts.
- * If the creation of a new chart ends successfully, this method emits the 'create' signal.
+ * If the creation of a new chart ends successfully, this method emits the 'create' signal. Otherwise it shows
+ * an error message and return null.
  *
  * @param chartType represents the chart's type, i.e. 'barchart', 'linechart', 'mapchart', 'table';
  * @param chartId the chart's ID;
- * @return 	a specific chart object.
+ * @return 	the created chart.
  */
 NorrisImpl.prototype.createChart = function(chartType, chartId) {
     // controllo che chartType sia in NorrisChart.factories -> lo fa già ChartImpl.createChart
@@ -79,16 +85,39 @@ NorrisImpl.prototype.createChart = function(chartType, chartId) {
         }
         return chart;
     }
+    else {
+        console.log("ERROR: this ID is already used.");
+        return null;
+    }
 };
 
+/**
+ * NorrisImpl.prototype.getChart allows you to get a Norris' chart.
+ *
+ * @param chartId contains the value of a chart's ID;
+ * @return 	the Norris' chart with the ID==chartId.
+ */
 NorrisImpl.prototype.getChart = function(chartId) {
    return this.charts[chartId];
 };
 
+/**
+ * NorrisImpl.prototype.getCharts allows you to get all Norris' charts.
+ *
+ * @return 	a list of the Norris' charts.
+ */
 NorrisImpl.prototype.getCharts = function() {
     return this.charts;
 };
 
+/**
+ * NorrisImpl.createPage allows you to create a new page and adds it to the Norris instance's list of pages.
+ * If the creation of a new page ends successfully, this method return the created page. Otherwise it shows
+ * an error message and return null.
+ *
+ * @param pageId the page's ID;
+ * @return  the created page.
+ */
 NorrisImpl.prototype.createPage = function(pageId) {
     if (!this.pages.hasOwnProperty(pageId)) {     //the page's ID should be unique
         var page = new NorrisPage(pageId);
@@ -96,12 +125,27 @@ NorrisImpl.prototype.createPage = function(pageId) {
         console.log("New page added: "+pageId); // TOGLIERE
         return page;
     }
+    else {
+        console.log("ERROR: this ID is already used.");
+        return null;
+    }
 };
 
+/**
+ * NorrisImpl.prototype.getPage allows you to get a Norris' page.
+ *
+ * @param pageId contains the value of a page's ID;
+ * @return 	the Norris' page with the ID==chartId.
+ */
 NorrisImpl.prototype.getPage = function(pageId){
     return this.pages[pageId];
 };
 
+/**
+ * NorrisImpl.prototype.getPages allows you to get all Norris' pages.
+ *
+ * @return 	a list of the Norris' pages.
+ */
 NorrisImpl.prototype.getPages = function(){
     return this.pages;
 };
