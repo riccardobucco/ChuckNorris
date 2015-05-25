bf97d2d1fcdc00af01c222d6e95c49dbf29162a8
Riccardo Bucco <riccardobucco@gmail.com>
2015-05-25 23:04:05 +0200

    aggiunto TULineChartInPlaceUpdater

--- Sources/Norris/test/unit/TULineChartInPlaceUpdater.js
+++ Sources/Norris/test/unit/TULineChartInPlaceUpdater.js
@@ -1,5 +1,5 @@
 /*
- * Name: {TUBarChartInPlaceUpdater.js}
+ * Name: {TULineChartInPlaceUpdater.js}
  * Module: {}
  * Location: {Norris/test/unit}
  * Date: {2015-05-25}
@@ -14,10 +14,10 @@
  * ================================================================================
  */
 
-var BarChartInPlaceUpdater = require('../../main/DataModel/NorrisChart/BarChartInPlaceUpdater.js');
+var LineChartInPlaceUpdater = require('../../main/DataModel/NorrisChart/LineChartInPlaceUpdater.js');
 var assert = require("assert");
 
-describe('BarChartInPlaceUpdater(id: String)', function(){
+describe('LineChartInPlaceUpdater(id: String)', function(){
 
 	describe('update(chart: ChartImpl, updateData: ChartUpdate): void', function(){
 		it('should reject chart without data',function(){
@@ -28,9 +28,9 @@ describe('BarChartInPlaceUpdater(id: String)', function(){
 			    { position: {x:0, y:0}, value: 'foo' },
 			    { position: {x:0, y:1}, value: 'foo' }
 			]
-			assert.throws(function () {(new BarChartInPlaceUpdater()).update(chart, newData)});
+			assert.throws(function () {(new LineChartInPlaceUpdater()).update(chart, newData)});
 		});
-		it('should update a given barchart with the inplace method',function(){
+		it('should update a given linechart with the inplace method',function(){
 			var chart = {
 				data: {
 				    datasets: [
@@ -53,7 +53,7 @@ describe('BarChartInPlaceUpdater(id: String)', function(){
 			        {values: [1,2,3,4]}
 				]
 			};
-			(new BarChartInPlaceUpdater()).update(chart, newData);
+			(new LineChartInPlaceUpdater()).update(chart, newData);
 			assert.deepEqual(updatedData, chart.data);
 		});
 	});
