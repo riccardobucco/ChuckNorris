8f11010c3b6e0152ba162351baa5280086817414
ChiaraBi <chiara.bigarella1@gmail.com>
2015-05-25 21:41:19 +0200

    DataModel: implementato table:stream

--- Sources/Norris/main/DataModel/NorrisChart/TableStreamUpdater.js
+++ Sources/Norris/main/DataModel/NorrisChart/TableStreamUpdater.js
@@ -25,6 +25,59 @@ TableStreamUpdater.getInstance = function() { // static
     return TableStreamUpdater.prototype.instance;
 };
 
+/**
+ * Updates a table with stream method. The table data should not be empty.
+ * @param {TableImpl} chart - the table to update;
+ * @param updateData - the updating.
+ */
 TableStreamUpdater.prototype.update = function (chart, updateData) {
-    chart.setData(updateData);
-};
+    var isEmpty=function(obj) {
+        for(var prop in obj) {
+            if(obj.hasOwnProperty(prop))
+                return false;
+        }
+        return true;
+    };
+
+    if (!isEmpty(updateData)) {
+        var data=chart.getData();
+        if (!isEmpty(data)) {
+            for(var i=0; i<updateData.length; i++) {
+                if (updateData[i].length==data.headers.length) {
+                    data.datasets.push(updateData[i]);
+                }
+                else {
+                    throw ("wrongUpdateData");
+                }
+            }
+            chart.setData(data);
+        }
+        else {
+            console.log("ERROR: the chart has no data to update.");
+            throw ("emptyChart");
+        }
+    }};
+
+/*
+
+ Table:
+ data = {
+    headers: ['anno','mese','giorno'], // intestazioni colonne
+    datasets: [
+        [ {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'},
+            {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'},
+            {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'}
+        ], // riga1
+        [ {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'},
+            {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'},
+            {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'}
+        ], // riga2
+    ]
+ }
+
+table:stream:
+    update= [
+        [{color: 'blue', background: 'white', value: '1'}, {color: 'blue', background: 'white', value: '1'}, {color: 'blue', background: 'white', value: '1'}], // riga 1
+        [{color: 'blue', background: 'white', value: '1'}, {color: 'blue', background: 'white', value: '1'}, {color: 'blue', background: 'white', value: '1'}] // riga 2
+    ] // aggiungo 2 righe nella posizione indicata nelle impostazioni (in testa o in coda)
+*/
