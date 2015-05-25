96bd8271f8245926272cff08e45f49c22ad12ade
ChiaraBi <chiara.bigarella1@gmail.com>
2015-05-25 18:59:17 +0200

    DataModel: implementato table:inplace

--- Sources/Norris/main/DataModel/NorrisChart/TableInPlaceUpdater.js
+++ Sources/Norris/main/DataModel/NorrisChart/TableInPlaceUpdater.js
@@ -26,6 +26,54 @@ TableInPlaceUpdater.getInstance = function() { // static
 };
 
 TableInPlaceUpdater.prototype.update = function (chart, updateData) {
-    // aggiornamento in place
-    chart.setData(updateData);
+    var isEmpty=function(obj) {
+        for(var prop in obj) {
+            if(obj.hasOwnProperty(prop))
+                return false;
+        }
+        return true;
+    };
+
+    if (!isEmpty(updateData)) {
+        var data=chart.getData();
+        if (!isEmpty(data)) {
+            for(var i=0; i<updateData.length; i++) {
+                var x=updateData[i].position.x;
+                var y=updateData[i].position.y;
+                data.datasets[x][y].color=updateData[i].data.color;
+                data.datasets[x][y].background=updateData[i].data.background;
+                data.datasets[x][y].value=updateData[i].data.value;
+            }
+            chart.setData(data);
+        }
+        else {
+            console.log("ERROR: the chart has no data to update.");
+            throw ("emptyChart");
+        }
+    }
 };
+
+/*
+
+ Table:
+ data = {
+    headers: ['anno','mese','giorno','ora'], // intestazioni colonne
+    datasets: [
+        [ {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'},
+            {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'},
+            {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'}
+        ], // riga1
+        [ {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'},
+            {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'},
+            {color : {r: 255, g: 255, b: 255}, background : {r: 255, g: 255, b: 255}, value: '1'}
+        ], // riga2
+    ]
+ }
+
+ table:inplace:
+ update = [
+    { position: {x:0, y:0}, data: {color: {r: 255, g: 255, b: 255}, background: {r: 255, g: 255, b: 255}, value:'foo'} },
+    { position: {x:0, y:1}, data: {color: {r: 255, g: 255, b: 255}, background: {r: 255, g: 255, b: 255}, value:'foo'} }
+ ] // modifico 2 valori già esistenti
+
+*/
