0fe73e2267afb71a3cf9ef4cd718259a3a223f73
Riccardo Bucco <riccardobucco@gmail.com>
2015-05-26 17:25:36 +0200

    Corretto errore sintassi

--- Sources/Norris/test/unit/TUNorrisBridge.js
+++ Sources/Norris/test/unit/TUNorrisBridge.js
@@ -23,7 +23,7 @@ describe('NorrisBridge', function(){
 
 	describe('createPage(pageId: String): void',function(){
 		it('should create a page with the indicated id',function(){
-		  	var page = {'page':{'charts': [],'settings': {'maxChartsCol': 3,'maxChartsRow': 2,'title': 'Norris's page'},'uid': '00'}};
+		  	var page = {'page':{'charts': [],'settings': {'maxChartsCol': 3,'maxChartsRow': 2,'title': 'Norris page'},'uid': '00'}};
 ;
 			
 			var norrisImpl = new NorrisImpl();
