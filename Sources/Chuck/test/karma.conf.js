946069aa5a3bcdb35bdb38209f84c08495f059ad
Riccardo Bucco <riccardobucco@gmail.com>
2015-05-26 15:19:09 +0200

    Aggiornata configurazione test

--- Sources/Chuck/test/karma.conf.js
+++ Sources/Chuck/test/karma.conf.js
@@ -5,11 +5,13 @@ module.exports = function(config){
 
     files : [
       'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/socket.io-client/socket.io.js',
       'bower_components/chartjs/Chart.min.js',
       'bower_components/openlayers/lib/OpenLayers.js',
       'bower_components/jquery/dist/jquery.min.js',
       'bower_components/datatables/media/js/jquery.dataTables.min.js',
+      'main/app.js',
       'main/model/norrischart/ChartImpl.js',
       'main/**/*.js',
       'test/unit/*.js'
