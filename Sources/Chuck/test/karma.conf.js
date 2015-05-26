744ecdb95ba91183b3d6faf38fd33b09c56464f4
Riccardo Bucco <riccardobucco@gmail.com>
2015-05-26 15:09:00 +0200

    Corretto file configurazione

--- Sources/Chuck/test/karma.conf.js
+++ Sources/Chuck/test/karma.conf.js
@@ -5,6 +5,12 @@ module.exports = function(config){
 
     files : [
       'bower_components/angular/angular.js',
+      'bower_components/socket.io-client/socket.io.js',
+      'bower_components/chartjs/Chart.min.js',
+      'bower_components/openlayers/lib/OpenLayers.js',
+      'bower_components/jquery/dist/jquery.min.js',
+      'bower_components/datatables/media/js/jquery.dataTables.min.js',
+      'main/model/norrischart/ChartImpl.js',
       'main/**/*.js',
       'test/unit/*.js'
     ],
