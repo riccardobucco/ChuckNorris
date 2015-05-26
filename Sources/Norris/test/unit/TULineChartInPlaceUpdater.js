8169c63dbdc73e046b7cb0856d92d1409549773e
Riccardo Bucco <riccardobucco@gmail.com>
2015-05-26 11:03:21 +0200

    modifiocati TU

--- Sources/Norris/test/unit/TULineChartInPlaceUpdater.js
+++ Sources/Norris/test/unit/TULineChartInPlaceUpdater.js
@@ -33,25 +33,27 @@ describe('LineChartInPlaceUpdater', function(){
 		it('should update a given linechart with the inplace method',function(){
 			var chart = {
 				data: {
-				    datasets: [
-				        {values: [1,2,3,4]},
-				        {values: [1,2,3,4]},
-				        {values: [1,2,3,4]}
+    				headers: ['uno','due','tre'],
+    				datasets: [
+				        [ {value: '1'}, {value: '2'}, {value: '3'} ],
+				        [ {value: '4'}, {value: '5'}, {value: '6'} ]
 				    ]
-				}
+ 				}
 			};
 			chart.getData = function() {return this.data;};
 			chart.setData = function(par) {this.data = par;};
 			var newData = [
-			    { position: {x:0, y:0}, value: 'foo' },
-			    { position: {x:0, y:1}, value: 'foo' }
-			];
+        		[ {value: '9999'}, {value: '8888'}, {value: '7777'} ],
+        		[ {value: '6666'}, {value: '5555'}, {value: '4444'} ]
+    		];
 			var updatedData = {
+				headers: ['uno','due','tre'],
 				datasets: [
-			        {values: ['foo','foo',3,4]},
-			        {values: [1,2,3,4]},
-			        {values: [1,2,3,4]}
-				]
+			        [ {value: '1'}, {value: '2'}, {value: '3'} ],
+			        [ {value: '4'}, {value: '5'}, {value: '6'} ],
+			        [ {value: '9999'}, {value: '8888'}, {value: '7777'} ],
+        			[ {value: '6666'}, {value: '5555'}, {value: '4444'} ]
+			    ]
 			};
 			(new LineChartInPlaceUpdater()).update(chart, newData);
 			assert.deepEqual(updatedData, chart.data);
---
+++ Sources/Norris/test/unit/TUTableStreamUpdater.js
@@ -0,0 +1,81 @@
+/*
+ * Name: {TUTableStreamUpdater.js}
+ * Module: {}
+ * Location: {Norris/test/unit}
+ * Date: {2015-05-26}
+ * Version: {v0.1}
+ *
+ * History:
+ *
+ * ================================================================================
+ * Version Date Programmer Changes
+ * ================================================================================
+ * 0.01 2015-05-26 Riccardo Bucco {creation}
+ * ================================================================================
+ */
+
+var TableStreamUpdater = require('../../main/DataModel/NorrisChart/TableStreamUpdater.js');
+var assert = require("assert");
+
+describe('TableStreamUpdater', function(){
+
+	describe('update(chart: ChartImpl, updateData: ChartUpdate): void', function(){
+		it('should not accept a chart without data',function(){
+			var chart = {data: {}};
+			chart.getData = function() {return this.data;};
+			chart.setData = function(data) {this.data = data;};
+			var newData = [
+		        [{value: '9999'}, {value: '8888'}, {value: '7777'}],
+		        [{value: '6666'}, {value: '5555'}, {value: '4444'}]
+			];
+			assert.throws(function () {(new TableStreamUpdater()).update(chart, newData)});
+		});
+		it('should reject an update whose data length is different from datasets length of the chart',function(){
+			var chart = {
+				data: {
+    				headers: ['col1','col2','col3'],
+				    datasets: [
+				        [ {value: '1'}, {value: '2'}, {value: '3'} ],
+				        [ {value: '1'}, {value: '1'}, {value: '1'} ]
+				    ]
+				 }
+			};
+			chart.getData = function() {return this.data;};
+			chart.setData = function(par) {this.data = par;};
+			var newData = [
+		        [ {value: '9999'}, {value: '8888'}, {value: '7777'}, {value: 'tooMuch1'} ],
+		        [ {value: '6666'}, {value: '5555'}, {value: '4444'}, {value: 'tooMuch2'} ]
+			];
+			assert.throws(function () {(new TableStreamUpdater()).update(chart, newData)});
+		});
+		it('should update a given chart with the stream method', function(){
+			var chart = {
+				data: {
+    				headers: ['col1','col2','col3'],
+				    datasets: [
+				        [ {value: '1'}, {value: '2'}, {value: '3'} ],
+				        [ {value: '1'}, {value: '1'}, {value: '1'} ]
+				    ]
+				 }
+			};
+			chart.getData = function() {return this.data;};
+			chart.setData = function(par) {this.data = par;};
+			var newData = [
+		        [ {value: '9999'}, {value: '8888'}, {value: '7777'} ],
+		        [ {value: '6666'}, {value: '5555'}, {value: '4444'} ]
+			];
+			var updatedData = {
+				headers: ['col1','col2','col3'],
+			    datasets: [
+			        [ {value: '1'}, {value: '2'}, {value: '3'} ],
+			        [ {value: '1'}, {value: '1'}, {value: '1'} ],
+			        [ {value: '9999'}, {value: '8888'}, {value: '7777'} ],
+		        	[ {value: '6666'}, {value: '5555'}, {value: '4444'} ]
+			    ]
+ 			};
+			(new TableStreamUpdater()).update(chart, newData);
+			assert.deepEqual(updatedData, chart.data);
+		});
+	});
+	
+});
