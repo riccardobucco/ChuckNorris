913c57066b9fac0b5078813acfcd5021a7b24349
Riccardo Bucco <riccardobucco@gmail.com>
2015-06-07 11:05:15 +0200

    Eliminato chuck (usare chuck-rtc)

--- Sources/Chuck/main/app.js
+++
@@ -1,5 +0,0 @@
-angular.module('chuck', ['chuck-requester', 'chuck-libs']);
-angular.module('chuck-requester', ['chuck-chart', 'chuck-libs']);
-angular.module('chuck-chart', ['norris-chartupdater']);
-angular.module('norris-chartupdater', []);
-angular.module('chuck-libs', []);
