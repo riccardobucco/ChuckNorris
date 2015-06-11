cc28b8e1a751b7187b159e5544a713dd05a508d1
ChiaraCar <chiara.carlon@gmail.com>
2015-06-11 23:29:23 +0200

    #188

--- Sources/Applicazione/test/unit/TUApplicazioneLineChartImpl.java
+++ Sources/Applicazione/test/unit/TUApplicazioneLineChartImpl.java
@@ -1,3 +1,18 @@
+/*
+* Name: TUApplicazioneLineChartImpl.java
+* Package: test.unit
+* Location: Sources/Applicazione/test/unit
+* Date: 2015-05-23
+* Version: v0.01
+*
+* History:
+* =================================================================
+* Version   Date    Programmer  Changes
+* =================================================================
+* v0.01 2015-05-23  Dal Bianco Davide  Creation
+* =================================================================
+*
+*/
 package test.unit;
 
 import junit.framework.TestCase;
@@ -5,9 +20,7 @@ import junit.framework.TestCase;
 import it.kaizenteam.app.model.NorrisChart.ChartImpl;
 import it.kaizenteam.app.model.NorrisChart.ChartModel;
 
-/**
- * Created by Moro on 11/06/15.
- */
+
 public class TUApplicazioneLineChartImpl extends TestCase {
     public void setUp() throws Exception {
         super.setUp();
--- Sources/Applicazione/test/unit/TUApplicazioneLineChartInPlaceUpdater.java
+++ Sources/Applicazione/test/unit/TUApplicazioneLineChartInPlaceUpdater.java
@@ -1,17 +1,15 @@
 /*
 * Name: TUApplicazioneLineChartInPlaceUpdater.java
-* Package: it.kaizenteam.app.model
+* Package: test.unit
 * Location: Sources/Applicazione/test/unit
-* Date: 2015-05-19
-* Version: v0.02
+* Date: 2015-05-23
+* Version: v0.01
 *
 * History:
 * =================================================================
 * Version	Date	Programmer	Changes
 * =================================================================
-* v0.02 2015-05-26  Moretto Alessandro   Verify
-* =================================================================
-* v0.01 2015-05-23  Davide Dal Bianco  Creation
+* v0.01 2015-05-23  Moretto Alessandro  Creation
 * =================================================================
 *
 */
