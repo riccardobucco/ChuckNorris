79ba3b2285481572c275523c58e827e6ea374770
ChiaraCar <chiara.carlon@gmail.com>
2015-06-11 22:41:12 +0200

    #188

--- Sources/Applicazione/test/unit/TUApplicazioneLineChartActivity.java
+++ Sources/Applicazione/test/unit/TUApplicazioneLineChartActivity.java
@@ -1,3 +1,18 @@
+/*
+* Name: TUApplicazioneLineChartActivity.java
+* Package: test.unit
+* Location: Sources/Applicazione/test/unit
+* Date: 2015-05-23
+* Version: v0.01
+*
+* History:
+* =================================================================
+* Version   Date    Programmer  Changes
+* =================================================================
+* v0.01 2015-05-23  Moretto Alessandro  Creation
+* =================================================================
+*
+*/
 package test.unit;
 
 import android.app.Activity;
@@ -9,9 +24,7 @@ import junit.framework.TestCase;
 import it.kaizenteam.app.R;
 import it.kaizenteam.app.view.LineChartActivity;
 
-/**
- * Created by Moro on 11/06/15.
- */
+
 public class TUApplicazioneLineChartActivity extends TestCase {
     Activity a=null;
     public void setUp() throws Exception {
