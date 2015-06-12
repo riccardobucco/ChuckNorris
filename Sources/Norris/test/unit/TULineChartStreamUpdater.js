6a6fba126ae2c7b8222252dfd78bd7bf5a3d2871
Riccardo Bucco <riccardobucco@gmail.com>
2015-06-12 16:12:24 +0200

    BASTA TABULAZIONI

--- Sources/Norris/test/unit/TULineChartStreamUpdater.js
+++ Sources/Norris/test/unit/TULineChartStreamUpdater.js
@@ -19,78 +19,78 @@ var assert = require("assert");
 
 describe('LineChartStreamUpdater', function(){
 
-	describe('update(chart: ChartImpl, updateData: ChartUpdate): void', function(){
-		it('should not accept a chart without data',function(){
-			var chart = {
-				data: {},
-				settings: {
-					maxPoints: 10
-				}
-			};
-			chart.getData = function() {return this.data;};
-			chart.setData = function(data) {this.data = data;};
-			chart.getSettings = function() {return this.settings;};
-			var newData = [
-			    {label: 'foo', data: [1,2,3,4,5]},
-    			{label: 'bar', data: [1,2,3,4,5]}
-			];
-			assert.throws(function () {(new LineChartStreamUpdater()).update(chart, newData)});
-		});
-		it('should reject an update whose data length is different from datasets length of the chart',function(){
-			var chart = {
-				data: {
-    				labels: ['2010','2011','2012','2013'],
-    				datasets: [
-        				{name: 'pippo', color : {r: 255, g: 255, b: 255}, values: [1,2,3,4]},
-        				{name: 'pluto', color : {r: 255, g: 255, b: 255}, values: [1,2,3,4]},
-        				{name: 'paperino', color : {r: 255, g: 255, b: 255}, values: [1,2,3,4]}
-    				]
- 				},
- 				settings: {
-					maxPoints: 10
-				}
-			};
-			chart.getData = function() {return this.data;};
-			chart.setData = function(par) {this.data = par;};
-			chart.getSettings = function() {return this.settings;};
-			var newData = { stream:[
-			    {label: 'foo', data: [1,2,3,4,5]},
-			    {label: 'bar', data: [1,2,3,4,5]}
-			]};
-			assert.throws(function () {(new LineChartStreamUpdater()).update(chart, newData)});
-		});
-		it('should update a given chart with the stream method', function(){
-			var chart = {
-				data: {
-    				labels: ['2010','2011','2012','2013'],
-    				datasets: [
-        				{values: [1,2,3,4]},
-        				{values: [1,2,3,4]},
-        				{values: [1,2,3,4]}
-    				]
- 				},
- 				settings: {
-					maxPoints: 10
-				}
-			};
-			chart.getData = function() {return this.data;};
-			chart.setData = function(par) {this.data = par;};
-			chart.getSettings = function() {return this.settings;};
-			var newData = { stream: [
-			    {label: 'foo', data: [777,888,999]},
-			    {label: 'bar', data: [111,222,333]}
-			]};
-			var updatedData = {
-				labels: ['2010','2011','2012','2013', 'foo', 'bar'],
-				datasets: [
-    				{values: [1,2,3,4,777,111]},
-    				{values: [1,2,3,4,888,222]},
-    				{values: [1,2,3,4,999,333]}
-				]
- 			};
-			(new LineChartStreamUpdater()).update(chart, newData);
-			assert.deepEqual(updatedData, chart.data);
-		});
-	});
-	
+    describe('update(chart: ChartImpl, updateData: ChartUpdate): void', function(){
+        it('should not accept a chart without data',function(){
+            var chart = {
+                data: {},
+                settings: {
+                    maxPoints: 10
+                }
+            };
+            chart.getData = function() {return this.data;};
+            chart.setData = function(data) {this.data = data;};
+            chart.getSettings = function() {return this.settings;};
+            var newData = [
+                {label: 'foo', data: [1,2,3,4,5]},
+                {label: 'bar', data: [1,2,3,4,5]}
+            ];
+            assert.throws(function () {(new LineChartStreamUpdater()).update(chart, newData)});
+        });
+        it('should reject an update whose data length is different from datasets length of the chart',function(){
+            var chart = {
+                data: {
+                    labels: ['2010','2011','2012','2013'],
+                    datasets: [
+                        {name: 'pippo', color : {r: 255, g: 255, b: 255}, values: [1,2,3,4]},
+                        {name: 'pluto', color : {r: 255, g: 255, b: 255}, values: [1,2,3,4]},
+                        {name: 'paperino', color : {r: 255, g: 255, b: 255}, values: [1,2,3,4]}
+                    ]
+                },
+                settings: {
+                    maxPoints: 10
+                }
+            };
+            chart.getData = function() {return this.data;};
+            chart.setData = function(par) {this.data = par;};
+            chart.getSettings = function() {return this.settings;};
+            var newData = { stream:[
+                {label: 'foo', data: [1,2,3,4,5]},
+                {label: 'bar', data: [1,2,3,4,5]}
+            ]};
+            assert.throws(function () {(new LineChartStreamUpdater()).update(chart, newData)});
+        });
+        it('should update a given chart with the stream method', function(){
+            var chart = {
+                data: {
+                    labels: ['2010','2011','2012','2013'],
+                    datasets: [
+                        {values: [1,2,3,4]},
+                        {values: [1,2,3,4]},
+                        {values: [1,2,3,4]}
+                    ]
+                },
+                settings: {
+                    maxPoints: 10
+                }
+            };
+            chart.getData = function() {return this.data;};
+            chart.setData = function(par) {this.data = par;};
+            chart.getSettings = function() {return this.settings;};
+            var newData = { stream: [
+                {label: 'foo', data: [777,888,999]},
+                {label: 'bar', data: [111,222,333]}
+            ]};
+            var updatedData = {
+                labels: ['2010','2011','2012','2013', 'foo', 'bar'],
+                datasets: [
+                    {values: [1,2,3,4,777,111]},
+                    {values: [1,2,3,4,888,222]},
+                    {values: [1,2,3,4,999,333]}
+                ]
+            };
+            (new LineChartStreamUpdater()).update(chart, newData);
+            assert.deepEqual(updatedData, chart.data);
+        });
+    });
+    
 });
--- Sources/Norris/test/unit/TUTableStreamUpdater.js
+++ Sources/Norris/test/unit/TUTableStreamUpdater.js
@@ -19,80 +19,80 @@ var assert = require("assert");
 
 describe('TableStreamUpdater', function(){
 
-	describe('update(chart: ChartImpl, updateData: ChartUpdate): void', function(){
-		it('should not accept a chart without data',function(){
-			var chart = {
-				data: {},
-				settings: {
-					maxRows: 10
-				}
-			};
-			chart.getData = function() {return this.data;};
-			chart.setData = function(data) {this.data = data;};
-			chart.getSettings = function() {return this.settings;};
-			var newData = [
-		        [{color: 'blue', background: 'white', value: '1'}, {color: 'blue', background: 'white', value: '1'}, 
-		            {color: 'blue', background: 'white', value: '1'}], 
-		        [{color: 'blue', background: 'white', value: '1'}, {color: 'blue', background: 'white', value: '1'}, 
-		            {color: 'blue', background: 'white', value: '1'}]
-			];
-			assert.throws(function () {(new TableStreamUpdater()).update(chart, newData)});
-		});
-		it('should reject an update whose data length is different from datasets length of the chart',function(){
-			var chart = {
-				data: {
-    				headers: ['col1','col2','col3'],
-				    datasets: [
-				        [ {value: '1'}, {value: '2'}, {value: '3'} ],
-				        [ {value: '1'}, {value: '1'}, {value: '1'} ]
-				    ]
-				},
-				settings: {
-					maxRows: 10
-				}
-			};
-			chart.getData = function() {return this.data;};
-			chart.setData = function(par) {this.data = par;};
-			chart.getSettings = function() {return this.settings;};
-			var newData = [
-		        [ {value: '9999'}, {value: '8888'}, {value: '7777'}, {value: 'tooMuch1'} ],
-		        [ {value: '6666'}, {value: '5555'}, {value: '4444'}, {value: 'tooMuch2'} ]
-			];
-			assert.throws(function () {(new TableStreamUpdater()).update(chart, newData)});
-		});
-		it('should update a given chart with the stream method', function(){
-			var chart = {
-				data: {
-    				headers: ['col1','col2','col3'],
-				    datasets: [
-				        {row: [ {value: '1'}, {value: '2'}, {value: '3'} ]},
-						{row: [ {value: '1'}, {value: '1'}, {value: '1'} ]}
-				    ]
-				},
-				settings: {
-					maxRows: 10
-				}
-			};
-			chart.getData = function() {return this.data;};
-			chart.setData = function(par) {this.data = par;};
-			chart.getSettings = function() {return this.settings;};
-			var newData = { stream : [
-				{ row: [ {value: '9999'}, {value: '8888'}, {value: '7777'} ]},
-				{ row: [ {value: '6666'}, {value: '5555'}, {value: '4444'} ]}
-			]};
-			var updatedData = {
-				headers: ['col1','col2','col3'],
-			    datasets: [
-			        // those are the expected values, because new values are inserted in top of the array   
-			        { row: [ {value: '6666'}, {value: '5555'}, {value: '4444'} ]},
-					{ row: [ {value: '9999'}, {value: '8888'}, {value: '7777'} ]},
-					{ row: [ {value: '1'}, {value: '2'}, {value: '3'} ]},
-					{ row: [ {value: '1'}, {value: '1'}, {value: '1'} ]}
-		        ]
- 			};
-			(new TableStreamUpdater()).update(chart, newData);
-			assert.deepEqual(updatedData, chart.data);
-		});
-	});
-	
+    describe('update(chart: ChartImpl, updateData: ChartUpdate): void', function(){
+        it('should not accept a chart without data',function(){
+            var chart = {
+                data: {},
+                settings: {
+                    maxRows: 10
+                }
+            };
+            chart.getData = function() {return this.data;};
+            chart.setData = function(data) {this.data = data;};
+            chart.getSettings = function() {return this.settings;};
+            var newData = [
+                [{color: 'blue', background: 'white', value: '1'}, {color: 'blue', background: 'white', value: '1'}, 
+                    {color: 'blue', background: 'white', value: '1'}], 
+                [{color: 'blue', background: 'white', value: '1'}, {color: 'blue', background: 'white', value: '1'}, 
+                    {color: 'blue', background: 'white', value: '1'}]
+            ];
+            assert.throws(function () {(new TableStreamUpdater()).update(chart, newData)});
+        });
+        it('should reject an update whose data length is different from datasets length of the chart',function(){
+            var chart = {
+                data: {
+                    headers: ['col1','col2','col3'],
+                    datasets: [
+                        [ {value: '1'}, {value: '2'}, {value: '3'} ],
+                        [ {value: '1'}, {value: '1'}, {value: '1'} ]
+                    ]
+                },
+                settings: {
+                    maxRows: 10
+                }
+            };
+            chart.getData = function() {return this.data;};
+            chart.setData = function(par) {this.data = par;};
+            chart.getSettings = function() {return this.settings;};
+            var newData = [
+                [ {value: '9999'}, {value: '8888'}, {value: '7777'}, {value: 'tooMuch1'} ],
+                [ {value: '6666'}, {value: '5555'}, {value: '4444'}, {value: 'tooMuch2'} ]
+            ];
+            assert.throws(function () {(new TableStreamUpdater()).update(chart, newData)});
+        });
+        it('should update a given chart with the stream method', function(){
+            var chart = {
+                data: {
+                    headers: ['col1','col2','col3'],
+                    datasets: [
+                        {row: [ {value: '1'}, {value: '2'}, {value: '3'} ]},
+                        {row: [ {value: '1'}, {value: '1'}, {value: '1'} ]}
+                    ]
+                },
+                settings: {
+                    maxRows: 10
+                }
+            };
+            chart.getData = function() {return this.data;};
+            chart.setData = function(par) {this.data = par;};
+            chart.getSettings = function() {return this.settings;};
+            var newData = { stream : [
+                { row: [ {value: '9999'}, {value: '8888'}, {value: '7777'} ]},
+                { row: [ {value: '6666'}, {value: '5555'}, {value: '4444'} ]}
+            ]};
+            var updatedData = {
+                headers: ['col1','col2','col3'],
+                datasets: [
+                    // those are the expected values, because new values are inserted in top of the array   
+                    { row: [ {value: '6666'}, {value: '5555'}, {value: '4444'} ]},
+                    { row: [ {value: '9999'}, {value: '8888'}, {value: '7777'} ]},
+                    { row: [ {value: '1'}, {value: '2'}, {value: '3'} ]},
+                    { row: [ {value: '1'}, {value: '1'}, {value: '1'} ]}
+                ]
+            };
+            (new TableStreamUpdater()).update(chart, newData);
+            assert.deepEqual(updatedData, chart.data);
+        });
+    });
+    
 });
