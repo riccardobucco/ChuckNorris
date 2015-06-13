ffe4735e6125d90a5a7381bf87b0f67626f21c40
ChiaraCar <chiara.carlon@gmail.com>
2015-06-13 18:14:55 +0200

    Aggiornati diari

--- Sources/Applicazione/main/java/it/kaizenteam/app/model/NorrisChart/MapChartElementInPlaceUpdate.java
+++ Sources/Applicazione/main/java/it/kaizenteam/app/model/NorrisChart/MapChartElementInPlaceUpdate.java
@@ -3,11 +3,17 @@
 * Package: it.kaizenteam.app.model.NorrisChart
 * Location: Sources/Applicazione/main/java/it/kaizenteam/app/model/NorrisChart
 * Date: 2015-05-2
-* Version: v0.02
+* Version: v1.00
 *
 * History:
 * =================================================================
 * Version	Date	Programmer	Changes
+* =================================================================
+* v1.00 2015-06-15  Carlon Chiara  Approved
+* =================================================================
+* v0.04 2015-06-02  Moretto Alessandro  Verify
+* =================================================================
+* v0.03 2015-05-30  Dal Bianco Davide Edit
 * =================================================================
 * v0.02 2015-05-26  Moretto Alessandro   Verify
 * =================================================================
