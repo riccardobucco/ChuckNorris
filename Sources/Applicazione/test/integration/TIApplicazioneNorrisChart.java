0282193bfa89199136a87647019cdebcc6923523
fpavanel <fpavanel93@gmail.com>
2015-06-13 19:23:33 +0200

    modificati diari TI applicazione

--- Sources/Applicazione/test/integration/TIApplicazioneNorrisChart.java
+++ Sources/Applicazione/test/integration/TIApplicazioneNorrisChart.java
@@ -3,11 +3,15 @@
 * Package: test.integration
 * Location: Sources/Applicazione/test/integration
 * Date: 2015-05-25
-* Version: v0.01
+* Version: v1.00
 *
 * History:
 * =================================================================
 * Version   Date    Programmer  Changes
+* =================================================================
+* v1.00 2015-06-15  Carlon Chiara  Approved
+* =================================================================
+* v0.02 2015-06-02  Bigarella Chiara  Verify
 * =================================================================
 * v0.01 2015-05-25  Dal Bianco Davide  Creation
 * =================================================================
